f6065572eb6df42ca725bfdb0072903921eb1bfd
jyuuuuu0 <s25023@gsm.hs.kr>
2025-11-08 23:59:03 +0900

    최댓값 만들기 (2)

---
+++ js/0/120862. 최댓값 만들기 (2)/최댓값 만들기 (2).js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+    let answer = Number.MIN_SAFE_INTEGER;;
+
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (answer < numbers[i] * numbers[j]) {
+                answer = numbers[i] * numbers[j];
+            }
+        }
+    }
+    return answer;
+}
