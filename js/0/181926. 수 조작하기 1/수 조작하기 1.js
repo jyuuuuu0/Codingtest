dc48a9b62f56ac4354a0285a8061b8595bb2b377
jyuuuuu0 <s25023@gsm.hs.kr>
2026-02-12 02:52:09 +0900

    수 조작하기 1

---
+++ js/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,11 @@
+function solution(n, control) {
+  let answer = n;
+  control.split("");
+  for (let i = 0; i < control.length; i++) {
+    if (control[i] == "w") answer += 1;
+    else if (control[i] == "s") answer -= 1;
+    else if (control[i] == "d") answer += 10;
+    else answer -= 10;
+  }
+  return answer;
+}
